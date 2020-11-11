78620a783554993e708a4877087dc04b1d503a2b
AlexandreDuval <57191018+AlexandreDuval@users.noreply.github.com>
2020-11-11 18:45:46 -0500

    Update start_stream.sh

--- start_stream.sh
+++ start_stream.sh
@@ -2,4 +2,4 @@
 gst-launch-1.0 -v v4l2src device=/dev/video0  \
 ! "image/jpeg,width=1280, height=720,framerate=30/1" \
 ! rtpjpegpay \
-! udpsink host=192.168.86.248 port=5000
+! udpsink host=IP_PC port=5000
