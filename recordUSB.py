8421c8f7525217f6b307365fafcbc2ba0b38c5b8
AlexandreDuval <57191018+AlexandreDuval@users.noreply.github.com>
2020-11-13 19:27:09 -0500

    Add files via upload

--- recordUSB.py
+++ recordUSB.py
@@ -1,6 +1,6 @@
 '''
-Ce programme ...
-
+Ce programme permet de filmer avec une ou plusieurs caméras USB
+branchées sur un Raspberry Pi et de les enregistrer sur la carte SD.
 
 RockÉTS Avionique
 A. Duval
@@ -44,57 +44,81 @@ class CameraUSB():
         def __init__(self, fps, resolution, numCam, camName):
                 self.fps = fps
                 self.resolution = resolution
+                self.numCam = numCam
                 self.camName = camName
-                self.cap = cv2.VideoCapture(numCam)
+                self.cap = cv2.VideoCapture(self.numCam)
                 self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                self.out = cv2.VideoWriter(self.camName+'.avi', self.fourcc, self.fps, self.resolution)
-
-
-
-# ------------------------------------------------------
-# Initialisation des cameras
-# ------------------------------------------------------
-cam1 = CameraUSB(30, (640, 480), 1, 'cam1')
-cams = [cam1]
-
+                self.out = cv2.VideoWriter(self.camName+'.mp4v', self.fourcc, self.fps, self.resolution)
 
 # ------------------------------------------------------
 # Fonctions
 # ------------------------------------------------------
-def convert_video(camList, remove=True, extension):
+def convert_video(camList):
         '''
-        
+        Fait la convertion du vidéo à l'extension souhaité.
+
+        Paramètres:
+                - camList (list) -- liste des caméras USB
+
+        Retour: n/a
+        Exceptions possibles: CoordException
         '''
-        t = time.localtime()
-        timestamp = time.strftime('%d-%b-%Y_%H:%M:%S', t)
-        for cam in camList:
-                command = "MP4Box -add {0}.avi {0}_{1}.{2}".format(cam.camName, timestamp, extension)
-                call([command], shell=True)
-                if remove:
-                        os.remove("{0}.avi".format(cam.camName))
+        if len(camList) > 0:
+                t = time.localtime()
+                timestamp = time.strftime('%d-%b-%Y_%H:%M:%S', t)
+                for cam in camList:
+                        #command = "MP4Box -add {0}.avi {0}_{1}.mp4".format(cam.camName, timestamp)
+                        #call([command], shell=True)
+                        #os.popen("ffmpeg -i '{input}.avi' -ac 2 -b:v 2000k -c:a aac -c:v \
+                        #libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 \
+                        #'{output}_{timestamp}.mp4'".format(input = cam.camName, output = cam.camName, timestamp=timestamp))
+                        #os.remove("{0}.avi".format(cam.camName))
+        else:
+                raise CoordException(F"<convert_video> Liste vide")
 
 def start_recording(camList):
         '''
+        Démarre l'enregistrement vidéos des caméras USB.
 
+        Paramètres:
+                - camList (list) -- liste des caméras USB
+
+        Retour: n/a
+        Exceptions possibles: CoordException
         '''
-        for cam in camList:
-                ret, frame = cam.cap.read()
-                cam.out.write(frame)
+        if len(camList) > 0:
+                for cam in camList:
+                        ret, frame = cam.cap.read()
+                        cam.out.write(frame)
+        else:
+                raise CoordException(F"<start_recording> Liste vide")
 
 def close(camList):
         '''
+        Ferme les caméras.
 
+        Paramètres:
+                - camList (list) -- liste des caméras USB
+
+        Retour: n/a
+        Exceptions possibles: CoordException
         '''
-        for cam in camList:
-                cam.cap.release()
-                cam.out.release()
+        if len(camList) > 0:
+                for cam in camList:
+                        cam.cap.release()
+                        cam.out.release()
+        else:
+                raise CoordException(F"<close> Liste vide")
 
 # ------------------------------------------------------
 # Fonction principale
 # ------------------------------------------------------
 def main():
+        # Initialisation des cameras
+        cam1 = CameraUSB(30, (640, 480), 0, 'cam1')
+        cam2 = CameraUSB(30, (640, 480), 1, 'cam2')
+        cams = [cam1, cam2]
         try:
-                os.chdir("/home/pi/Documents/RockETS/VideosCaptured")
                 while True:
                         start_recording(cams)
                         msg = input()
@@ -102,7 +126,7 @@ def main():
                                 convert_video(cams)
                                 break
                         else:
-                                print(''stop' pour arreter')
+                                print("'stop' pour arreter")
 
                 close(cams)
 
@@ -118,5 +142,5 @@ def main():
 # ------------------------------------------------------
 # Programme principal
 # ------------------------------------------------------
-if if __name__ == "__main__":
+if __name__ == "__main__":
     main()
